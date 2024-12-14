60696bc7876ed0b53a3f78a8068880f6c6ce0e8f
reiser02 <reiser976@outlook.es>
2024-12-15 00:40:29 +0100

    Optización del código y control de errores argumentos URIs

--- flask/server.py
+++ flask/server.py
@@ -2,66 +2,110 @@
 
 import boto3
 import json
-from flask import Flask, jsonify, Response
-from boto3.dynamodb.conditions import Attr
-import numpy as np
+from flask import Flask, Response, request
+from boto3.dynamodb.conditions import key
+import statistics
 
 app = Flask(__name__)
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('proy-stats')
 
+def get_yearmonth():
+    month, year = request.args.get('month'), request.args.get('year')
+    try:
+        month = int(month)
+        year = int(year)
+    except (ValueError, TypeError):
+        return None
+    return year * 100 + month
+
+def build_json_response(msg, status):
+    return Response(msg, status=status, mimetype='application/json')
+
+def build_ok_json_response(msg):
+    return build_json_response(msg, 200)
+
+def get_invalid_args_response():
+    http_code = 400  # Bad Request
+    response_json = {
+        'error': 'Invalid arguments. Make sure \'month\' and \'year\' are provided and are valid.'
+    }
+    return build_json_response(str(response_json), http_code)
+
+def get_not_found_response():
+    http_code = 404  # Not Found
+    response_json = {
+        'error': 'Combination of month and year not found in DB.'
+    }
+    return build_json_response(str(response_json), http_code)
+
 @app.route('/maxdiff')
 def maxdiff():
-    yearmonth = request.args.get('YearMonth') 
+    yearmonth = get_yearmonth()
+
+    if yearmonth is None:
+        return get_invalid_args_response()
 
     response = table.query(
-        FilterExpression=Attr('YearMonth').eq(yearmonth)
+        KeyConditionExpression=Key('YearMonth').eq(yearmonth)
     )
 
-    if reponse.get("Items") is None:
-        Response("{'a':'b'}", status=404, mimetype='application/json')
-    else:
-        temperatures_mean = [t['Mean'] for t in response['Items']]
+    items = response.get('Items', [])
 
-    return jsonify(max(temperatures_mean) - min(temperatures_mean))
+    if len(items) == 0:
+        return get_not_found_response()
+
+    temperatures_mean = [t['Mean'] for t in items]
+    result = {
+        'result': max(temperatures_mean) - min(temperatures_mean)
+    }
+    return build_ok_json_response(str(result))
 
 @app.route('/sd')
 def sd():
-    yearmonth = request.args.get('YearMonth') 
+    yearmonth = get_yearmonth()
+
+    if yearmonth is None:
+        return get_invalid_args_response()
 
     response = table.query(
-        FilterExpression=Attr('YearMonth').eq(yearmonth)
+        KeyConditionExpression=Key('YearMonth').eq(yearmonth)
     )
 
-    if reponse.get("Items") is None:
-        Response("{'a':'b'}", status=404, mimetype='application/json')
-    else:
-        temperatures_Deviation = [t['Deviation'] for t in response]
+    items = response.get('Items', [])
+
+    if len(items) == 0:
+        return get_not_found_response()
     
+    temperatures_deviation = [t['Deviation'] for t in items]
+    result = {
+        'result': max(temperatures_deviation)
+    }
+    return build_ok_json_response(str(result))
 
-    Deviation = max(temperatures_Deviation)
-
-    return jsonify(Deviation)
-
-@app.route('/temp'):
+@app.route('/temp')
 def temp():
-    yearmonth = request.args.get('YearMonth') 
+    yearmonth = get_yearmonth()
+
+    if yearmonth is None:
+        return get_invalid_args_response()
 
     response = table.query(
-        FilterExpression=Attr('YearMonth').eq(yearmonth)
+        KeyConditionExpression=Key('YearMonth').eq(yearmonth)
     )
 
-    if reponse.get("Items") is None:
-        Response("{'a':'b'}", status=404, mimetype='application/json')
-    else:
-        temperatures_mean = [t['Mean'] for t in response]
+    items = response.get('Items', [])
 
-    
+    if len(items) == 0:
+        return get_not_found_response()
 
-    return np.mean(temperatures_mean)
+    temperatures_mean = [t['Mean'] for t in items]
+    result = {
+        'result': statistics.mean(temperatures_mean)
+    }
+    return build_ok_json_response(str(result))
 
-
-if __name__ == '__main__':)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
 
